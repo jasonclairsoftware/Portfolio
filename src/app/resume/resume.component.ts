ac83560a098f3c378e36d580cbf08b679047ac37
Jason Clair <jasonclairgamedev@gmail.com>
2024-04-24 14:12:45 -0700

    Added dummy resume to assets and updated the resume component

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,16 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { AccordionModule } from 'ngx-bootstrap/accordion';
 
 @Component({
   selector: 'app-resume',
   standalone: true,
-  imports: [],
+  imports: [AccordionModule],
   templateUrl: './resume.component.html',
   styleUrl: './resume.component.scss'
 })
 export class ResumeComponent {
 
-  constructor(private titleservice: Title){
+  constructor(private titleservice: Title, private renderer: Renderer2){
     titleservice.setTitle("Jason Clair | Resume");
+  }
+
+  downloadFile() {
+    const link = this.renderer.createElement('a');
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', '../../assets/resume.docx');
+    link.setAttribute('download', 'resume.docx');
+    link.click();
+    link.remove();
   }
 }
