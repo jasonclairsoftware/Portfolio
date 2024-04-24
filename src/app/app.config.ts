a9edb984acc7286c4973968df1390f29496d1fac
Jason Clair <jasonclairgamedev@gmail.com>
2024-04-23 17:47:26 -0700

    Created basic modal for project-card

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,8 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { BsModalService } from 'ngx-bootstrap/modal';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), BsModalService]
 };
--- src/app/project-card/project-card.component.ts
+++ src/app/project-card/project-card.component.ts
@@ -1,14 +1,34 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, NgModule, TemplateRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Project } from '../_models/project';
+import { BsModalRef, BsModalService, ModalModule, ModalOptions } from 'ngx-bootstrap/modal';
+import { ProjectModalComponent } from '../project-modal/project-modal.component';
+
 
 @Component({
   selector: 'app-project-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ModalModule],
   templateUrl: './project-card.component.html',
   styleUrl: './project-card.component.scss'
 })
+
+
 export class ProjectCardComponent {
   @Input() project = {} as Project;
+  bsModalRef?: BsModalRef;
+
+  constructor(private modalService: BsModalService) { }
+
+
+  openProjectModal() {
+    const modalOptions: ModalOptions = {
+      class: 'modal-lg',
+      initialState: {
+        project: this.project
+      }
+    };
+
+    this.bsModalRef = this.modalService.show(ProjectModalComponent, modalOptions);
+  }
 }
