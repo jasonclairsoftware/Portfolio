83e044e34f3a028f248002c10765febc9b140308
Jason Clair <jasonclairgamedev@gmail.com>
2024-04-24 08:39:23 -0700

    Updated the modal for new information and updated the featured project on the home page.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,11 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { ProjectsService } from '../_services/projects.service';
 import { Project } from '../_models/project';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CarouselModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
--- src/app/project-modal/project-modal.component.ts
+++ src/app/project-modal/project-modal.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { BsModalRef } from 'ngx-bootstrap/modal';
 import { Project } from '../_models/project';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-project-modal',
   standalone: true,
-  imports: [CarouselModule],
+  imports: [CarouselModule, CommonModule],
   templateUrl: './project-modal.component.html',
   styleUrl: './project-modal.component.scss'
 })
